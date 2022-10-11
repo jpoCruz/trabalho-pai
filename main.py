f7a7c75847c5e169395fa2053eb4b43b0485f7a5
Pedro Rodrigues de Freitas Faria <74972110+Overseer-coder@users.noreply.github.com>
2022-10-11 17:47:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from re import X
 import tkinter as tk
 from tkinter import filedialog
 import os
 from PIL import Image, ImageTk
 import mouse
 from pynput import mouse #pip install pynput
+import cv2 #pip install opencv-python
 
 
 #
@@ -33,38 +35,57 @@ def comando():
     pass
 
 def abrir_imagem():
+    global file
+
     file = filedialog.askopenfilename(initialdir=os.getcwd(), title = "Escolha a imagem", filetypes=(("PNG File", "*.png"), ("JPG File", "*.jpg"), ("All Files", "*.*")))
     if file:
         img = Image.open(file)
-        tam, tam = img.size
+        tam, tam = img.size # Recuperando tamanho da imagem para seu posicionamento na interface
         img = ImageTk.PhotoImage(img)
         imagem_label.configure(image=img)
         imagem_label.image=img
         imagem_label.grid(column = 1, row = 0)
         if tam == 224:
-            imagem_label.place(x=86, y=80) # imagem 224x224
+            imagem_label.place(x=86, y=80) # Imagem 224x224
         else :
-            imagem_label.place(x=49, y=43) # imagem 299x299
-        
+            imagem_label.place(x=49, y=43) # Imagem 299x299
+
+
+coord = []# Armazena as coordenadas de corte da imagem
+def recorte_imagem(event, x, y, flags, param):
+	global coord
+
+	if event == cv2.EVENT_LBUTTONDOWN:# Se o botão esquerdo do mouse foi segurado, x e y iniciais são gravados
+		coord = [(x, y)]
+	elif event == cv2.EVENT_LBUTTONUP:# Verifica se soltou o clique do botão esquerdo do mouse, x e y finais são gravados
+		coord.append((x, y))		
+		cv2.rectangle(imgCrop, coord[0], coord[1], 0, 2)# Desenha um retângulo na região marcada de acordo com (x, y) inciais e finais 
 
 def recorte():
-    with mouse.Listener(on_click=on_click) as coordenadas:
-        coordenadas.join()
-    with mouse.Listener(on_click=on_click) as coordenadas:
-        coordenadas.join()
-        
-        
+    global imgCrop
+    imgCrop = cv2.imread(file)
+    clone = imgCrop.copy()# Um clone da imagem original é criado para que possamos mudar a região de corte se necessário, 
+                          # e a imagem original estará sem alterações pois as manipulações de corte ocorrerão no clone
+    cv2.namedWindow("Recorte de Imagem")# Label para identifação da tela de recorte
+    cv2.setMouseCallback("Recorte de Imagem", recorte_imagem)# Função de "escutar" o mouse
 
-def on_click(x, y, button, pressed):
-    if pressed:
-        #mouse.get_position()
-        print(x, y)
-        return False
+    while True:
+        cv2.imshow("Recorte de Imagem", imgCrop)# Mostra a imagem e espera a tecla correta para recortar
+        key = cv2.waitKey(1)
+
+        if key == ord("0"):# Apertando "0" no teclado, o corte é resetado e outro pode ser escolhido
+            imgCrop = clone.copy()# Para recuperar a imagem sem o retângulo, recuperamos pelo clone
+        elif key == ord("1"):# Apertando "1" no teclado, a imagem é cortada
+            break
+
+    if len(coord) == 2:# Se houverem duas coordenadas e saimos do loop, a imagem foi cortada
+        corte = clone[coord[0][1]:coord[1][1], coord[0][0]:coord[1][0]]# Corte é feito na imagem clone a partir das coordenadas armazenadas
+        cv2.imshow("Corte", corte)
+        cv2.waitKey(0);
+
+    cv2.imwrite('crop.png', corte)# Imagem cortada é salva no diretório local
 
 
-        
-
-#Menu
 menu = tk.Menu(root)
 root.config(menu = menu)
 
@@ -96,15 +117,5 @@ imagem_label = tk.Label(image=imagem)
 imagem_label.image = imagem
 imagem_label.grid(column = 1, row = 0)
 imagem_label.place(x=86, y=80)
-
-
-#def on_click(x, y, button, pressed):
-    #if not pressed and button == mouse.Button.left:
-       # print(x, y)
-
-#with mouse.Listener(on_click=on_click) as coordenadas:
-    #coordenadas.join()
-
-
 
 root.mainloop()
